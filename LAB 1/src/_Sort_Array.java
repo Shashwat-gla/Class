3782ac78fd19324e8846633ce3cff2ef7bc61194
Shashwat-gla <rajeevhaoi@gmail.com>
2026-08-09 01:09:50 +0530

    Store changes

--- LAB 1/src/_Sort_Array.java
+++ LAB 1/src/_Sort_Array.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class _Sort_Array {
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
-        int x [] = {12,3,4,5,6,7,89};
-        boolean flag=true;
-        for (int i = 0;i<x.length-1;i++){
-                if (x[i]>x[i+1]){
-                    flag=false;
+        int x [] = { 12 , 3 , 4 , 5 , 6 , 7 , 89 };
+        boolean flag = true;
+        for (int i = 0 ; i <x.length - 1 ; i++ ){
+                if ( x[i] > x[ i + 1 ] ){
+                    flag = false;
                     break ;
                 }
         }
