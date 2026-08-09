571530b277fca1075ffd9d3cda9cd1ab5dee0d2e
Shashwat-gla <rajeevhaoi@gmail.com>
2026-08-09 23:35:24 +0530

    Store changes

--- LAB 1/src/Left_rotate.java
+++ LAB 1/src/Left_rotate.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Left_rotate {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
@@ -8,7 +9,6 @@ public class Left_rotate {
             arr[i]=arr[i+1];
         }
         arr[arr.length-1] = first ;
-        
         for (int i = 0;i< arr.length;i++){
             System.out.print(arr[i]);
         }
